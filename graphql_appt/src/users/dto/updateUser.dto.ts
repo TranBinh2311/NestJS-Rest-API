813cd3949fc4293870501f209acac5c7e809c232
TranBinh2311 <binhlinh473@gmail.com>
2021-11-02 17:45:40 +0700

    2-11-2021

--- graphql_appt/src/users/dto/updateUser.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/graphql';
-import { IsString, IsEmail, IsEnum } from 'class-validator';
-import { createUserDTO } from './createUser.dto';
-
-export class updateUserDTO extends PartialType(createUserDTO) {
-
-}
--- graphql_appt/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersResolver } from './users.resolver';
-import { PrismaService } from 'src/prisma/prisma.service';
-
-@Module({
-    providers: [UsersService, UsersResolver, PrismaService],
-})
-export class UsersModule {}
